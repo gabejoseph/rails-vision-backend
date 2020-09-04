d9542dcc65767a2464836cb76042866f7a378947
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-04 15:25:51 -0400

    added listings and user scaffolds

---
+++ db/migrate/20200904191508_create_listings.rb
@@ -0,0 +1,17 @@
+class CreateListings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :listings do |t|
+      t.string :img
+      t.string :location
+      t.string :title
+      t.string :description
+      t.float :star
+      t.float :price
+      t.float :total
+      t.belongs_to :user
+      
+
+      t.timestamps
+    end
+  end
+end
