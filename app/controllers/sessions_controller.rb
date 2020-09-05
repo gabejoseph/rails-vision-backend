38159c34db3d8236de39fb03dee250ee0304c88f
gabejoseph <gabriel.joseph4@gmail.com>
2020-09-05 19:11:17 -0400

    authentication on the backend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 class SessionsController < ApplicationController
+
     def create
-        @user = User.find_by(name: params[:user][:name])
+        @user = User.find_by(name: params[:user][:email])
         if @user && @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            render json: {
+                status: :created,
+                logged_in: true,
+                user: user
+            }
         else 
-            redirect_to new_session_path
+            render json: { status: 401 }
         end 
     end 
+
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :hosts
   resources :listings
   resources :users
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
