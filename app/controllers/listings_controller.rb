556c20528f696fb35c252af76f2ef093ac7acf12
gabejoseph <gabriel.joseph4@gmail.com>
2020-12-16 10:10:55 -0500

    removed star from listings controller strong params

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -46,6 +46,6 @@ class ListingsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def listing_params
-      params.require(:listing).permit(:img, :location, :title, :description, :star, :price)
+      params.require(:listing).permit(:img, :location, :title, :description, :price)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :username, :email, :password, :host)
+      params.require(:user).permit(:name, :username, :email, :password)
     end
 end
